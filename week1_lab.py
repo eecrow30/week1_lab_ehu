a0a820241aa2ba25caaa97400ea46519595d2a4e
eecrow30 <26493101@edgehill.ac.uk>
2025-10-07 15:22:46 +0100

    Add files via upload

---
+++ week1_lab.py
@@ -0,0 +1,19 @@
+
+#### print message
+print("hello world")
+
+
+#### storing strings and integers as variables
+student_name = "Erin"
+student_age = 18
+
+print(f"Student Name: {student_name}")
+print(f"Student Age: {student_age}")
+
+
+#### simple arithmetic
+num1 = 20
+num2 = 25
+total = num1+num2
+
+print(f"the sum is {total}")
